aab595449948a7b8f773da9f8f4abfd1c776554f
Malik-Ahsan-wq <ahsanmalikking57@gmail.com>
2025-04-19 11:13:48 +0500

    gfd

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -157,12 +157,12 @@ const About = () => {
         <div className="pb-10  pt-10 max-w-7xl ml-20 ">
           <Marquee>
             <div className="flex   gap-20 border-b border-t border-gray-400 w-full justify-center items-center p-5 ">
-              <img src="pulic/logo1.svg" alt="" />
-              <img src="public/logo2.svg" alt="" />
-              <img src="public/logo3.svg" alt="" />
-              <img src="public/logo4.svg" alt="" />
-              <img src="public/logo5.svg" alt="" />
-              <img src="public/logo6.svg" alt="" />
+              <img src="/logo1.svg" alt="" />
+              <img src="/logo2.svg" alt="" />
+              <img src="/logo3.svg" alt="" />
+              <img src="/logo4.svg" alt="" />
+              <img src="/logo5.svg" alt="" />
+              <img src="/logo6.svg" alt="" />
             </div>
           </Marquee>
         </div>
