7bc98bef7e10a1cc35f3711467948641e45edc22
Malik-Ahsan-wq <ahsanmalikking57@gmail.com>
2025-04-19 14:51:07 +0500

    dfdasfsd

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -120,36 +120,47 @@ const About = () => {
         <img className="rounded-2xl" src="/video.png" alt="" />
       </div>
 
-      <div className="flex flex-col md:py-20  md:flex-row w-full max-w-7xl gap-10">
-        {/* Left Column */}
-        <div className="flex flex-col gap-5 justify-center items-center md:items-start w-full md:w-1/2">
-          <h3 className="text-2xl font-bold text-orange-400">Resume</h3>
-          <h1 className="text-4xl md:text-5xl text-center md:text-left font-bold">
-            <span className="text-orange-400">A</span>ll over my details is
-            here...
-          </h1>
-          <div className="flex flex-col gap-3 w-full px-4 md:px-10 ">
-            {tabs.map((tab, index) => (
-              <button
-                onClick={() => setActiveTab(index)}
-                key={`tab_${index}`}
-                className={`w-full rounded-lg border py-4 font-bold md:py-4 md:border md:bg-white md:text-black md:font-bold md:text-start md:px-4 md:w-[80%] ${
-                  activeTab === index ? "bg-black text-white" : ""
-                } hover:bg-black hover:text-white transition`}
-              >
-                {tab}
-              </button>
-            ))}
+      <div className="md:flex md:justify-center  ">
+        {" "}
+        <div className="  md:py-20  md:flex-row w-full max-w-7xl gap-10">
+          {/* Left Column */}
+          <div className="flex flex-col gap-5 justify-center items-center md:items-start w-full md:w-1/2">
+            <h3 className="text-2xl  md:ml-40 font-bold text-orange-400">
+              Resume
+            </h3>
+            <h1 className="text-4xl md:text-6xl text-center md:text-center   md:w-300  font-bold">
+              <span className="text-orange-400 ">A</span>ll over my details is
+              here...
+            </h1>
+            <div className=" gap-10 md:flex    md:ml-20  px-4 md:px-5 md:gap-5 justify-center md:w-250 ">
+              {tabs.map((tab, index) => (   
+                <button
+                  onClick={() => setActiveTab(index)}
+                  key={`tab_${index}`}
+                  className={`w-full mt-5  rounded-lg border py-4 font-bold md:py-4 md:border md:bg-white md:text-black md:font-bold md:text-start md:px-4 md:w-[80%] ${
+                    activeTab === index ? "bg-black text-white" : ""
+                  } hover:bg-black hover:text-white transition`}
+                >
+                  {tab}
+                </button>
+              ))}
+            </div>
           </div>
-        </div>
 
-        {/* Right Column */}
-        <div className="w-full md:w-1/2 px-4">
-          {activeTab === 0 && <About1 />}
-          {activeTab === 1 && <Experience />}
-          {activeTab === 2 && <Education />}
-          {activeTab === 3 && <Skills />}
-          {activeTab === 4 && <Awards />}
+          {/* Right Column */}
+          <div className=" mt-10 md:flex justify-center gap-50">
+            {" "}
+            <div className="  md:mt-40 md:w-200">
+              <img src="/about1.png" alt="" />
+            </div>
+            <div className="w-full  px-4">
+              {activeTab === 0 && <About1 />}
+              {activeTab === 1 && <Experience />}
+              {activeTab === 2 && <Education />}
+              {activeTab === 3 && <Skills />}
+              {activeTab === 4 && <Awards />}
+            </div>
+          </div>
         </div>
       </div>
 
@@ -168,67 +179,67 @@ const About = () => {
         </div>
       </div>
 
-       <footer className=" animate__animated animate__fadeInRight px-6 md:px-20 py-16 bg-black text-white pt-30">
-              <div className="flex flex-col md:flex-row justify-between gap-16">
-                {/* Left Section */}
-                <div className="flex-1">
-                  <h1 className="text-5xl md:text-7xl font-bold leading-tight text-white">
-                    let<span className="text-orange-400">'s</span> work together
-                  </h1>
-                  <p className="mt-4 text-lg font-semibold text-gray-400">
-                    Product Designer <span className="text-white">|</span> Based in
-                    Pakistan
-                  </p>
-      
-                  <div className="flex gap-6 text-2xl mt-6 text-white">
-                    <FaBehance className="hover:text-orange-500 transition cursor-pointer" />
-                    <FaLinkedinIn className="hover:text-orange-500 transition cursor-pointer" />
-                    <IoBasketballOutline className="hover:text-orange-500 transition cursor-pointer" />
-                    <FaGithub className="hover:text-orange-500 transition cursor-pointer" />
-                  </div>
-                </div>
-      
-                {/* Right Section */}
-                <div className="flex flex-col gap-6 md:flex-row md:gap-8 mt-10">
-                  {/* Contact Card 1 */}
-                  <div className="bg-black shadow-md border border-gray-700 rounded-2xl p-6 w-full md:w-80 transition hover:shadow-lg flex flex-col justify-between h-full">
-                    <div>
-                      <p className="text-white font-bold text-2xl mb-6">
-                        Looking for a high-tech designer?
-                      </p>
-                    </div>
-                    <p className="flex items-center gap-3 font-semibold text-2xl text-orange-400 cursor-pointer">
-                      hello@ahsan.com <MdOutlineArrowOutward />
-                    </p>
-                  </div>
-      
-                  {/* Contact Card 2 */}
-                  <div className="bg-black-900 shadow-md border border-gray-700 rounded-2xl p-6 w-full md:w-80 transition hover:shadow-lg flex flex-col justify-between h-full">
-                    <div>
-                      <p className="text-white text-2xl font-bold mb-6">
-                        Have a project in mind?
-                      </p>
-                    </div>
-                    <p className="flex items-center gap-3 font-semibold text-2xl text-orange-400 cursor-pointer">
-                      hello@ahsan.com <MdOutlineArrowOutward />
-                    </p>
-                  </div>
-                </div>
+      <footer className=" animate__animated animate__fadeInRight px-6 md:px-20 py-16 bg-black text-white pt-30">
+        <div className="flex flex-col md:flex-row justify-between gap-16">
+          {/* Left Section */}
+          <div className="flex-1">
+            <h1 className="text-5xl md:text-7xl font-bold leading-tight text-white">
+              let<span className="text-orange-400">'s</span> work together
+            </h1>
+            <p className="mt-4 text-lg font-semibold text-gray-400">
+              Product Designer <span className="text-white">|</span> Based in
+              Pakistan
+            </p>
+
+            <div className="flex gap-6 text-2xl mt-6 text-white">
+              <FaBehance className="hover:text-orange-500 transition cursor-pointer" />
+              <FaLinkedinIn className="hover:text-orange-500 transition cursor-pointer" />
+              <IoBasketballOutline className="hover:text-orange-500 transition cursor-pointer" />
+              <FaGithub className="hover:text-orange-500 transition cursor-pointer" />
+            </div>
+          </div>
+
+          {/* Right Section */}
+          <div className="flex flex-col gap-6 md:flex-row md:gap-8 mt-10">
+            {/* Contact Card 1 */}
+            <div className="bg-black shadow-md border border-gray-700 rounded-2xl p-6 w-full md:w-80 transition hover:shadow-lg flex flex-col justify-between h-full">
+              <div>
+                <p className="text-white font-bold text-2xl mb-6">
+                  Looking for a high-tech designer?
+                </p>
               </div>
-      
-              {/* Footer Branding */}
-              <h1 className=" text-center text-6xl md:text-8xl font-bold mt-24 border-b border-gray-700 pb-4 text-[#ffe9d9]">
-                Ahsan Bashir
-              </h1>
-      
-              {/* Bottom Info */}
-              <div className="flex flex-col md:flex-row justify-between items-center mt-10 text-sm text-gray-500">
-                <p>© 2023 All rights reserved</p>
-                <button className="flex items-center gap-2 mt-2 md:mt-0 hover:text-white transition">
-                  Back to top <MdOutlineArrowOutward />
-                </button>
+              <p className="flex items-center gap-3 font-semibold text-2xl text-orange-400 cursor-pointer">
+                hello@ahsan.com <MdOutlineArrowOutward />
+              </p>
+            </div>
+
+            {/* Contact Card 2 */}
+            <div className="bg-black-900 shadow-md border border-gray-700 rounded-2xl p-6 w-full md:w-80 transition hover:shadow-lg flex flex-col justify-between h-full">
+              <div>
+                <p className="text-white text-2xl font-bold mb-6">
+                  Have a project in mind?
+                </p>
               </div>
-            </footer>
+              <p className="flex items-center gap-3 font-semibold text-2xl text-orange-400 cursor-pointer">
+                hello@ahsan.com <MdOutlineArrowOutward />
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Footer Branding */}
+        <h1 className=" text-center text-6xl md:text-8xl font-bold mt-24 border-b border-gray-700 pb-4 text-[#ffe9d9]">
+          Ahsan Bashir
+        </h1>
+
+        {/* Bottom Info */}
+        <div className="flex flex-col md:flex-row justify-between items-center mt-10 text-sm text-gray-500">
+          <p>© 2023 All rights reserved</p>
+          <button className="flex items-center gap-2 mt-2 md:mt-0 hover:text-white transition">
+            Back to top <MdOutlineArrowOutward />
+          </button>
+        </div>
+      </footer>
     </div>
   );
 };
