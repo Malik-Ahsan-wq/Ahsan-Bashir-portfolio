45a7b84a12d66a08589ff522f0860b95bdb92126
Malik-Ahsan-wq <ahsanmalikking57@gmail.com>
2025-04-19 15:02:42 +0500

    fsdfs

--- src/Components/BlogSection.jsx
+++ src/Components/BlogSection.jsx
@@ -5,117 +5,129 @@ import { MdOutlineArrowOutward } from "react-icons/md";
 const BlogSection = () => {
   return (
     <div>
-      <div className="py-20">
+      <div className="py-20 px-4">
         <h3 className="text-center text-3xl font-bold text-orange-400">Blog</h3>
-        <h1 className="text-center text-6xl font-bold mb-20">My Blog Post</h1>
+        <h1 className="text-center text-4xl md:text-6xl font-bold mb-12">
+          My Blog Post
+        </h1>
 
-        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 md:mt-20">
-            <img className="w-full" src="/blog1.png" alt="blog1" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
+        <div className="flex  justify-center gap-10 ">
+          {/* Blog Post 1 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-10">
+            <img className="md:w-150 rounded-lg" src="/blog1.png" alt="blog1" />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
               Right-to-left behind development in mobile web design
             </p>
           </div>
 
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 md:mt-20">
-            <img className="w-full" src="/blog2.png" alt="blog2" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
-              Right-to-left behind development in mobile web design
+          {/* Blog Post 2 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-30">
+            <img className="md:w-150 rounded-lg" src="/blog2.png" alt="blog2" />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
+              Understanding color theory in UI components
             </p>
           </div>
 
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 md:mt-20">
-            <img className="w-full" src="/blog3.png" alt="blog3" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
-              Right-to-left behind development in mobile web design
+          {/* Blog Post 3 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-10">
+            <img
+              className="md:w-150  rounded-lg"
+              src="/blog3.png"
+              alt="blog3"
+            />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
+              Creating accessible design for everyone
             </p>
           </div>
 
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 ">
-            <img className="w-full" src="/blog4.png" alt="blog4" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
-              Right-to-left behind development in mobile web design
+          {/* Blog Post 4 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-30">
+            <img
+              className="md:w-150  rounded-lg"
+              src="/blog4.png"
+              alt="blog4"
+            />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
+              How minimalism improves user experience
             </p>
           </div>
         </div>
       </div>
-      <div className="py-20">
-        
 
-        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 md:mt-30">
-            <img className="w-full" src="/blog1.png" alt="blog1" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
+      <div className="py-20 px-4">
+      
+      
+
+        <div className="flex  justify-center gap-10 ">
+          {/* Blog Post 1 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-10">
+            <img className="md:w-150 rounded-lg" src="/blog1.png" alt="blog1" />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
               Right-to-left behind development in mobile web design
             </p>
           </div>
 
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 md:mt-30">
-            <img className="w-full" src="/blog2.png" alt="blog2" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
-              Right-to-left behind development in mobile web design
+          {/* Blog Post 2 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-30">
+            <img className="md:w-150 rounded-lg" src="/blog2.png" alt="blog2" />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
+              Understanding color theory in UI components
             </p>
           </div>
 
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 ">
-            <img className="w-full" src="/blog3.png" alt="blog3" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
-              Right-to-left behind development in mobile web design
+          {/* Blog Post 3 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-10">
+            <img
+              className="md:w-150  rounded-lg"
+              src="/blog3.png"
+              alt="blog3"
+            />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
+              Creating accessible design for everyone
             </p>
           </div>
 
-          <div className="w-full max-w-sm flex flex-col items-start mt-10 md:mt-30">
-            <img className="w-full" src="/blog4.png" alt="blog4" />
-            <span className="mt-5 text-sm">
-              <span className="mr-1">•</span>UI Design
-            </span>
-            <span className="text-lg text-orange-400 mt-1">
-              <span className="mr-1">•</span>03 May 2019
-            </span>
-            <p className="text-2xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-5">
-              Right-to-left behind development in mobile web design
+          {/* Blog Post 4 */}
+          <div className="w-full sm:w-[45%] lg:w-[22%] mt-30">
+            <img
+              className="md:w-150  rounded-lg"
+              src="/blog4.png"
+              alt="blog4"
+            />
+            <div className="mt-3 flex justify-between text-sm text-gray-500">
+              <span>UI Design</span>
+              <span className="text-orange-400">03 May 2019</span>
+            </div>
+            <p className="text-xl hover:text-orange-400 duration-150 cursor-pointer font-bold mt-3">
+              How minimalism improves user experience
             </p>
           </div>
         </div>
