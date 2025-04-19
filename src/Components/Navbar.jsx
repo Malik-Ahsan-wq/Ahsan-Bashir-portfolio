eb521254b7dc2e3abe704715aeefcafc4796cb75
Malik-Ahsan-wq <ahsanmalikking57@gmail.com>
2025-04-19 11:53:59 +0500

    sadf

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -152,7 +152,7 @@ const Navbar = () => {
         </ul>
 
         {/* Social Icons */}
-        <span className="flex gap-6 text-3xl mt-7">
+        <span className="flex gap-6 text-3xl ">
           <FaBehance className="hover:text-amber-600 duration-150 cursor-pointer" />
           <FaLinkedinIn className="hover:text-amber-600 duration-150 cursor-pointer" />
           <IoBasketballOutline className="hover:text-amber-600 duration-150 cursor-pointer" />
